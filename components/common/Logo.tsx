1bcc2534d24bd0b3d8109f1e9f5a8742fe763dbb
TOGB-Reeper <acebany080@gmail.com>
2024-11-16 16:44:40 +0200

    logo update

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import Link from "next/link";
-import Image from "next/image"
+import { FaPills } from "react-icons/fa";
 
 const Logo = () => {
     return (
         <div>
             <Link href={"/"} className="flex justify-center items-center">
-                <Image
-                    src="/logo.png"
-                    alt="site logo"
-                    width={90}
-                    height={90}
-                    priority
-                />
+                <FaPills className='w-8 h-8 text-green-600 mr-2'/>
                 <h2 className="uppercase space-x-2 sm:text-2xl text-lg sm:font-semibold font-medium">
                     <span className="hover:text-ceruleanBlue hoverEffect text-green-600">Ebenezer</span>
                     <span className="hover:text-limeGreen hoverEffect text-blue-500">Pharmacy</span>
